a5a482df4b1754a84bc80caca20a86b88557ac5a
LoDeFi31 <117044448+LoDeFi31@users.noreply.github.com>
2025-11-17 10:41:31 +0100

    Fix .well-known manifest serving

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ['assets.coingecko.com', 'coin-images.coingecko.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
